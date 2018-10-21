7e3af29fbda92285c83e44253cc261df665af20b
Hector <vix.hector@gmail.com>
2018-10-21 03:32:56 -0600

    Nuevos campos Fuente

--- cdc-modelo/src/main/java/cdc/com/api/modelo/Fuente.java
+++ cdc-modelo/src/main/java/cdc/com/api/modelo/Fuente.java
@@ -6,6 +6,7 @@
 package cdc.com.api.modelo;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -19,6 +20,8 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -36,6 +39,38 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "Fuente.findByCita", query = "SELECT f FROM Fuente f WHERE f.cita = :cita")
     , @NamedQuery(name = "Fuente.findByArchivado", query = "SELECT f FROM Fuente f WHERE f.archivado = :archivado")})
 public class Fuente implements Serializable {
+
+    @Column(name = "publicacioncdc")
+    private Boolean publicacioncdc;
+
+    @Column(name = "cobgeo")
+    private String cobgeo;
+    @Column(name = "coords")
+    private String coords;
+    @Column(name = "coordn")
+    private String coordn;
+    @Column(name = "coorde")
+    private String coorde;
+    @Column(name = "coordo")
+    private String coordo;
+    @Column(name = "resumen")
+    private String resumen;
+    @Column(name = "valor")
+    private String valor;
+    @Column(name = "clave")
+    private String clave;
+    @Column(name = "comentario")
+    private String comentario;
+    @Column(name = "notadigest")
+    private String notadigest;
+    @Column(name = "actualizar")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date actualizar;
+    @Column(name = "control")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date control;
+    @Column(name = "bcd")
+    private String bcd;
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -145,6 +180,119 @@ public class Fuente implements Serializable {
     @Override
     public String toString() {
         return "cdc.com.api.modelo.Fuente[ fuenteId=" + fuenteId + " ]";
+    }
+
+    public String getCobgeo() {
+        return cobgeo;
+    }
+
+    public void setCobgeo(String cobgeo) {
+        this.cobgeo = cobgeo;
+    }
+
+    public String getCoords() {
+        return coords;
+    }
+
+    public void setCoords(String coords) {
+        this.coords = coords;
+    }
+
+    public String getCoordn() {
+        return coordn;
+    }
+
+    public void setCoordn(String coordn) {
+        this.coordn = coordn;
+    }
+
+    public String getCoorde() {
+        return coorde;
+    }
+
+    public void setCoorde(String coorde) {
+        this.coorde = coorde;
+    }
+
+    public String getCoordo() {
+        return coordo;
+    }
+
+    public void setCoordo(String coordo) {
+        this.coordo = coordo;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getNotadigest() {
+        return notadigest;
+    }
+
+    public void setNotadigest(String notadigest) {
+        this.notadigest = notadigest;
+    }
+
+    public Date getActualizar() {
+        return actualizar;
+    }
+
+    public void setActualizar(Date actualizar) {
+        this.actualizar = actualizar;
+    }
+
+    public Date getControl() {
+        return control;
+    }
+
+    public void setControl(Date control) {
+        this.control = control;
+    }
+
+    public String getBcd() {
+        return bcd;
+    }
+
+    public void setBcd(String bcd) {
+        this.bcd = bcd;
+    }
+
+    public Boolean getPublicacioncdc() {
+        return publicacioncdc;
+    }
+
+    public void setPublicacioncdc(Boolean publicacioncdc) {
+        this.publicacioncdc = publicacioncdc;
     }
     
 }
